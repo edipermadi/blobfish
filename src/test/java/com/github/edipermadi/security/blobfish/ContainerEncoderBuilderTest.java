e39d714ff571b178bde6e36734826d0663ba6eb2
Edi Permadi <edipermadi@gmail.com>
2017-09-15 23:01:47 +0800

    enhance unit test
    Signed-off-by: Edi Permadi <edipermadi@gmail.com>

--- src/test/java/com/github/edipermadi/security/blobfish/ContainerEncoderBuilderTest.java
+++ src/test/java/com/github/edipermadi/security/blobfish/ContainerEncoderBuilderTest.java
@@ -115,27 +115,23 @@ public final class ContainerEncoderBuilderTest extends AbstractTest {
                 .setOutputStream(null);
     }
 
-    @Parameters({"keystore-entry-password",
-            "keystore-alias-sig-sender",
+    @Parameters({"keystore-alias-sig-sender",
             "keystore-alias-enc-sender",
             "keystore-alias-enc-receiver1",
             "keystore-alias-enc-receiver2",
             "blobfish-password"})
     @Test(expectedExceptions = IllegalStateException.class)
-    public void whenSigningPrivateKeyNotSetThenBuildingThrowsException(final String entryPassword,
-                                                                       final String senderSigningAlias,
+    public void whenSigningPrivateKeyNotSetThenBuildingThrowsException(final String senderSigningAlias,
                                                                        final String senderEncryptionAlias,
                                                                        final String recipient1EncryptionAlias,
                                                                        final String recipient2EncryptionAlias,
                                                                        final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
-        final PrivateKey privateKey = (PrivateKey) keyStore.getKey(senderSigningAlias, entryPassword.toCharArray());
         final X509Certificate senderSigningCertificate = (X509Certificate) keyStore.getCertificate(senderSigningAlias);
         final X509Certificate senderEncryptionCertificate = (X509Certificate) keyStore.getCertificate(senderEncryptionAlias);
         final X509Certificate recipient1EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient1EncryptionAlias);
         final X509Certificate recipient2EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient2EncryptionAlias);
         try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             new ContainerEncoderBuilder()
-                    //.setSigningKey(privateKey)
                     .setSigningCertificate(senderSigningCertificate)
                     .addRecipientCertificate(senderEncryptionCertificate)
                     .addRecipientCertificate(recipient1EncryptionCertificate)
@@ -160,14 +156,12 @@ public final class ContainerEncoderBuilderTest extends AbstractTest {
                                                                         final String recipient2EncryptionAlias,
                                                                         final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
         final PrivateKey privateKey = (PrivateKey) keyStore.getKey(senderSigningAlias, entryPassword.toCharArray());
-        final X509Certificate senderSigningCertificate = (X509Certificate) keyStore.getCertificate(senderSigningAlias);
         final X509Certificate senderEncryptionCertificate = (X509Certificate) keyStore.getCertificate(senderEncryptionAlias);
         final X509Certificate recipient1EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient1EncryptionAlias);
         final X509Certificate recipient2EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient2EncryptionAlias);
         try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             new ContainerEncoderBuilder()
                     .setSigningKey(privateKey)
-                    //.setSigningCertificate(senderSigningCertificate)
                     .addRecipientCertificate(senderEncryptionCertificate)
                     .addRecipientCertificate(recipient1EncryptionCertificate)
                     .addRecipientCertificate(recipient2EncryptionCertificate)
@@ -189,22 +183,39 @@ public final class ContainerEncoderBuilderTest extends AbstractTest {
                                                                   final String senderEncryptionAlias,
                                                                   final String recipient1EncryptionAlias,
                                                                   final String recipient2EncryptionAlias,
-                                                                  final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
+                                                                  final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
         final PrivateKey privateKey = (PrivateKey) keyStore.getKey(senderSigningAlias, entryPassword.toCharArray());
         final X509Certificate senderSigningCertificate = (X509Certificate) keyStore.getCertificate(senderSigningAlias);
         final X509Certificate senderEncryptionCertificate = (X509Certificate) keyStore.getCertificate(senderEncryptionAlias);
         final X509Certificate recipient1EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient1EncryptionAlias);
         final X509Certificate recipient2EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient2EncryptionAlias);
+        new ContainerEncoderBuilder()
+                .setSigningKey(privateKey)
+                .setSigningCertificate(senderSigningCertificate)
+                .addRecipientCertificate(senderEncryptionCertificate)
+                .addRecipientCertificate(recipient1EncryptionCertificate)
+                .addRecipientCertificate(recipient2EncryptionCertificate)
+                .setPassword(password)
+                .build();
+    }
+
+    @Parameters({"keystore-entry-password",
+            "keystore-alias-sig-sender",
+            "blobfish-password"})
+    @Test(expectedExceptions = IllegalStateException.class)
+    public void whenEncryptionCertificateIsEmptyThenBuildingThrowsException(final String entryPassword,
+                                                                            final String senderSigningAlias,
+                                                                            final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
+        final PrivateKey privateKey = (PrivateKey) keyStore.getKey(senderSigningAlias, entryPassword.toCharArray());
+        final X509Certificate senderSigningCertificate = (X509Certificate) keyStore.getCertificate(senderSigningAlias);
         try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            new ContainerEncoderBuilder()
+            final ContainerEncoder encoder = new ContainerEncoderBuilder()
                     .setSigningKey(privateKey)
                     .setSigningCertificate(senderSigningCertificate)
-                    .addRecipientCertificate(senderEncryptionCertificate)
-                    .addRecipientCertificate(recipient1EncryptionCertificate)
-                    .addRecipientCertificate(recipient2EncryptionCertificate)
                     .setPassword(password)
-                    //.setOutputStream(baos)
+                    .setOutputStream(baos)
                     .build();
+            Assert.assertNotNull(encoder);
         }
     }
 
@@ -246,15 +257,13 @@ public final class ContainerEncoderBuilderTest extends AbstractTest {
             "keystore-alias-sig-sender",
             "keystore-alias-enc-sender",
             "keystore-alias-enc-receiver1",
-            "keystore-alias-enc-receiver2",
-            "blobfish-password"})
+            "keystore-alias-enc-receiver2"})
     @Test
     public void whenPasswordNotSetThenBuildingCommenced(final String entryPassword,
                                                         final String senderSigningAlias,
                                                         final String senderEncryptionAlias,
                                                         final String recipient1EncryptionAlias,
-                                                        final String recipient2EncryptionAlias,
-                                                        final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
+                                                        final String recipient2EncryptionAlias) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
         final PrivateKey privateKey = (PrivateKey) keyStore.getKey(senderSigningAlias, entryPassword.toCharArray());
         final X509Certificate senderSigningCertificate = (X509Certificate) keyStore.getCertificate(senderSigningAlias);
         final X509Certificate senderEncryptionCertificate = (X509Certificate) keyStore.getCertificate(senderEncryptionAlias);
@@ -267,7 +276,38 @@ public final class ContainerEncoderBuilderTest extends AbstractTest {
                     .addRecipientCertificate(senderEncryptionCertificate)
                     .addRecipientCertificate(recipient1EncryptionCertificate)
                     .addRecipientCertificate(recipient2EncryptionCertificate)
-                    //.setPassword(password)
+                    .setOutputStream(baos)
+                    .build();
+            Assert.assertNotNull(encoder);
+        }
+    }
+
+    @Parameters({"keystore-entry-password",
+            "keystore-alias-sig-sender",
+            "keystore-alias-enc-sender",
+            "keystore-alias-enc-receiver1",
+            "keystore-alias-enc-receiver2",
+            "blobfish-password"})
+    @Test
+    public void whenAllParameterSetThenBuildingCommenced(final String entryPassword,
+                                                         final String senderSigningAlias,
+                                                         final String senderEncryptionAlias,
+                                                         final String recipient1EncryptionAlias,
+                                                         final String recipient2EncryptionAlias,
+                                                         final String password) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchPaddingException, CertificateEncodingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException {
+        final PrivateKey privateKey = (PrivateKey) keyStore.getKey(senderSigningAlias, entryPassword.toCharArray());
+        final X509Certificate senderSigningCertificate = (X509Certificate) keyStore.getCertificate(senderSigningAlias);
+        final X509Certificate senderEncryptionCertificate = (X509Certificate) keyStore.getCertificate(senderEncryptionAlias);
+        final X509Certificate recipient1EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient1EncryptionAlias);
+        final X509Certificate recipient2EncryptionCertificate = (X509Certificate) keyStore.getCertificate(recipient2EncryptionAlias);
+        try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            final ContainerEncoder encoder = new ContainerEncoderBuilder()
+                    .setSigningKey(privateKey)
+                    .setSigningCertificate(senderSigningCertificate)
+                    .addRecipientCertificate(senderEncryptionCertificate)
+                    .addRecipientCertificate(recipient1EncryptionCertificate)
+                    .addRecipientCertificate(recipient2EncryptionCertificate)
+                    .setPassword(password)
                     .setOutputStream(baos)
                     .build();
             Assert.assertNotNull(encoder);
